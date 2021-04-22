ba5418f601d3f99d6826c64084cc115cdeef1ce5
Henriette Holm <henriette.aamand.holm@live.dk>
2021-04-22 13:52:43 +0200

    Tilføjet min testklasse

---
+++ Microwave.Test.Integration/IT1_CookControllerToDisplayPowerTubeTimer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using NSubstitute;
+
+namespace Microwave.Test.Integration
+{
+    public class IT1_CookControllerToDisplayPowerTubeTimer
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
