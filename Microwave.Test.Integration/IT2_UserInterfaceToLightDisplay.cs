932a7ccf68ae00a1fb0ef2843121240c22ebc1a6
Sndergaard <emilsndergaard>
2021-04-22 15:27:51 +0200

    IT2 oprettet

---
+++ Microwave.Test.Integration/IT2_UserInterfaceToLightDisplay.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace Microwave.Test.Integration
+{
+    public class IT2_UserInterfaceToLightDisplay
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
