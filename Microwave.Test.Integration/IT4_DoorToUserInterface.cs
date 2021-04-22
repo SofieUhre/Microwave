2b9453f613dcaa3dc87a50602a6fadd62978ff75
SofieUhre <60734443+SofieUhre@users.noreply.github.com>
2021-04-22 14:03:41 +0200

    IT4_DoorToUserInterface - testprojekt oprettet

---
+++ Microwave.Test.Integration/IT4_DoorToUserInterface.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Microwave.Test.Integration
+{
+    class IT4_DoorToUserInterface
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
