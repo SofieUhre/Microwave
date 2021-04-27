04a268d047d3363856ab86c4c1cbf7349e517317
SofieUhre <60734443+SofieUhre@users.noreply.github.com>
2021-04-27 17:12:26 +0200

    Oprettet setup til IT4_DoorToUserInterface

--- Microwave.Test.Integration/IT4_DoorToUserInterface.cs
+++ Microwave.Test.Integration/IT4_DoorToUserInterface.cs
@@ -3,20 +3,49 @@ using System.Collections.Generic;
 using System.Text;
 using NSubstitute;
 using NUnit.Framework;
+using Microwave.Classes.Interfaces;
+using Microwave.Classes.Boundary;
+using Microwave.Classes.Controllers;
 
 namespace Microwave.Test.Integration
 {
     class IT4_DoorToUserInterface
     {
+        private IDoor _sut;
+        private ITimer _fakeTimer;
+        private IOutput _fakeOutput;
+        private IButton _fakeButton;
+        private IPowerTube _powerTube;
+        private IDisplay _display;
+        private ILight _light;
+        private ICookController _cookController;
+        private IUserInterface _userInterface;
+       
+
         [SetUp]
         public void Setup()
         {
+            // Definerer system under test
+            _sut = new Door();
+
+            //Definerer fakes
+            _fakeTimer = Substitute.For<ITimer>();
+            _fakeOutput = Substitute.For<IOutput>();
+            _fakeButton = Substitute.For<IButton>();
+
+            //Definerer de rigtige klasser der skal bruges 
+            _powerTube = new PowerTube(_fakeOutput);
+            _display = new Display(_fakeOutput);
+            _light = new Light(_fakeOutput);
+            _cookController = new CookController(_fakeTimer, _display, _powerTube);
+            _userInterface = new UserInterface(_fakeButton, _fakeButton, _fakeButton, _sut, _display, _light, _cookController);
         }
 
         [Test]
-        public void Test1()
+        public void OpenDoor_EventRaised_ExpectedOutput()
         {
-            Assert.Pass();
+            
+            _sut.Open();
         }
     }
 }
