c35e44a8e21c3a197cf13210fd99017ec142a3c9
Mads Emil <58051332+memilnieAU@users.noreply.github.com>
2021-05-05 09:06:43 +0200

    Tilføjelse i Cookerens interface, UI public set

--- Microwave.Test.Integration/IT5_ButtonsToUserInterface.cs
+++ Microwave.Test.Integration/IT5_ButtonsToUserInterface.cs
@@ -40,12 +40,13 @@ namespace Microwave.Test.Integration
             timer = new Timer();
             powerTube = new PowerTube(fakeOutput);
 
+            cooker = new CookController(timer, display, powerTube);
             ui = new UserInterface(powerButton, timeButton, startCancelButton, door, display, light, cooker);
-            cooker = new CookController(timer, display, powerTube,ui);
+            cooker.UI = ui;
         }
 
         [Test]
-        public void Test5_1_1_PressPowerBut_AssertShowPower()
+        public void Test5_1_1_PressPowerBut_AssertOnOutputLineShowPower()
         {
             //Act (Is ind State =Ready)
             powerButton.Press();
@@ -55,7 +56,7 @@ namespace Microwave.Test.Integration
         }
 
         [Test]
-        public void Test5_1_2_PressPowerBut_AssertShowPower()
+        public void Test5_1_2_PressPowerBut_AssertOnOutputLineShowPower()
         {
             //Act (Is ind State =Ready)
             powerButton.Press();
@@ -66,7 +67,7 @@ namespace Microwave.Test.Integration
 
         }
         [Test]
-        public void Test5_2_PressTimeBut_AssertShowTime()
+        public void Test5_2_PressTimeBut_AssertOnOutputLineShowTime()
         {
             //Arrange - Need to be in state = SetPower
             powerButton.Press();
@@ -80,7 +81,7 @@ namespace Microwave.Test.Integration
         }
 
         [Test]
-        public void Test5_3_1_PressStartCancelBut_AssertTurnOnAnd50W()
+        public void Test5_3_1_PressStartCancelBut_AssertOnOutputLineTurnOnAnd50W()
         {
             //Arrange - Need to be in state = SetTime
             powerButton.Press();
@@ -91,42 +92,13 @@ namespace Microwave.Test.Integration
 
             //Assert
             fakeOutput.Received(1).OutputLine(Arg.Is<string>("Light is turned on"));
-            fakeOutput.Received(1).OutputLine(Arg.Is<string>("PowerTube works with 50"));
+            fakeOutput.Received(1).OutputLine(Arg.Is<string>("Display shows: 50 W"));
 
         }
         [Test]
-        public void Test5_3_2_PressStartCancelBut_AssertTurnOnAnd350W()
+        public void Test5_3_2_PressStartCancelBut_AssertOnOutputLineTurnOnAnd350W()
         {
             //Arrange - Need to be in state = SetTime
-            powerButton.Press();
-            powerButton.Press();
-            powerButton.Press();
-            powerButton.Press();
-            powerButton.Press();
-            powerButton.Press();
-            powerButton.Press();
-            timeButton.Press();
-
-            //Act - Is in State = SetTime
-            startCancelButton.Press();
-
-            //Assert
-            fakeOutput.Received(1).OutputLine(Arg.Is<string>("Light is turned on"));
-            fakeOutput.Received(1).OutputLine(Arg.Is<string>("PowerTube works with 350"));
-
-        }
-        [Test]
-        public void Test5_3_3_PressStartCancelBut_AssertTurnOnAnd750W()
-        {
-            //Arrange - Need to be in state = SetTime
-            powerButton.Press();
-            powerButton.Press();
-            powerButton.Press();
-            powerButton.Press();
-            powerButton.Press();
-            powerButton.Press();
-            powerButton.Press();
-            powerButton.Press();
             powerButton.Press();
             powerButton.Press();
             powerButton.Press();
@@ -141,12 +113,41 @@ namespace Microwave.Test.Integration
 
             //Assert
             fakeOutput.Received(1).OutputLine(Arg.Is<string>("Light is turned on"));
-            fakeOutput.Received(1).OutputLine(Arg.Is<string>("PowerTube works with 750"));
+            fakeOutput.Received(1).OutputLine(Arg.Is<string>("Display shows: 350 W"));
+
+        }
+        [Test]
+        public void Test5_3_3_PressStartCancelBut_AssertOnOutputLineTurnOnAnd750W()
+        {
+            //Arrange - Need to be in state = SetTime
+            powerButton.Press();
+            powerButton.Press();
+            powerButton.Press();
+            powerButton.Press();
+            powerButton.Press();
+            powerButton.Press();
+            powerButton.Press();
+            powerButton.Press();
+            powerButton.Press();
+            powerButton.Press();
+            powerButton.Press();
+            powerButton.Press();
+            powerButton.Press();
+            powerButton.Press();
+            powerButton.Press();
+            timeButton.Press();
+
+            //Act - Is in State = SetTime
+            startCancelButton.Press();
+
+            //Assert
+            fakeOutput.Received(1).OutputLine(Arg.Is<string>("Light is turned on"));
+            fakeOutput.Received(1).OutputLine(Arg.Is<string>("Display shows: 750 W"));
 
         }
 
         [Test]
-        public void Test5_4_PressStartCancelBut_AssertTurnOff()
+        public void Test5_4_PressStartCancelBut_AssertOnOutputLineTurnOff()
         {
             //Arrange - Need to be in state = Cooking
             powerButton.Press();
